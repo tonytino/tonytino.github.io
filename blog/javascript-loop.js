9959d652da7de79778db4971c14dde7bc87d72eb
Tonytino <tony91139@Gmail.com>
2015-04-12 16:40:32 -0700

    Add Cult Blog WK7 and Updates

--- blog/javascript-loop.js
+++ blog/javascript-loop.js
@@ -10,24 +10,7 @@ javascriptLoop.map (function (number) {
   else
       console.log(number + " is lame.");
 })
-// Output:
-// 1 is lame.
-// 2 is lame.
-// Fizz
-// 4 is lame.
-// Buzz
-// Fizz
-// 7 is lame.
-// 8 is lame.
-// Fizz
-// Buzz
-// 11 is lame.
-// Fizz
-// 13 is lame.
-// 14 is lame.
-// FizzBuzz
 
-// Example: Creating your own for loop in Javascript
 
 var javascriptLoop = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
@@ -40,7 +23,10 @@ for (var index = 0; index < javascriptLoop.length; index++)
       console.log("Buzz");
   else
       console.log(javascriptLoop[index] + " is lame.");
-// // Output:
+
+
+
+// Output:
 // 1 is lame.
 // 2 is lame.
 // Fizz
